a79591a55b6b502c05ea9c9ace29c9b49e300bf5
Sean Schade <sean.schade@myhomepay.com>
2019-08-19 14:25:18 -0500

    Create Source to encapsulate msslq connection

--- data.go
+++ data.go
@@ -1,5 +1,27 @@
 package main
 
 import (
-	_ "github.com/denisenkom/go-mssqldb"
+	"database/sql"
+
+	mssql "github.com/denisenkom/go-mssqldb"
 )
+
+func NewSource(dsn string) (*Source, error) {
+	// Create a new connector object by calling NewConnector
+	connector, err := mssql.NewConnector(dsn)
+	if err != nil {
+		return nil, err
+	}
+	return &Source{
+		db: sql.OpenDB(connector),
+	}, nil
+}
+
+// Source.
+type Source struct {
+	db *sql.DB
+}
+
+func (s *Source) Close() error {
+	return s.db.Close()
+}
